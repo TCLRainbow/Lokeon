1d84b4a2ce525e8a0bfe70a5b989b33494df2e48
Pythonic-Rainbow <wongchingho0@gmail.com>
2020-07-12 18:01:10 +0800

    Start new Bungeecord project

--- src/main/java/chingdim/lokeon/Lokeon.java
+++ src/main/java/chingdim/lokeon/Lokeon.java
@@ -1,50 +1,16 @@
 package chingdim.lokeon;
 
-import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;
+import net.md_5.bungee.api.plugin.Plugin;
 
-import java.util.concurrent.ExecutionException;
-
-public class Lokeon extends JavaPlugin{
-    private String ip;
-    private Http http;
-
-    private void build_ip(String host) {
-        ip = String.format("http://%s:80/", host);
-    }
-
-    @Override
-    public void onLoad() {
-        super.onLoad();
-        AWS aws = new AWS();
-        getLogger().info("Checking Europe server instance IP");
-        if (this.getConfig().getBoolean("debug")) build_ip("localhost");
-        else {
-            try {
-                build_ip(aws.getInstanceIP());
-            } catch (ExecutionException | InterruptedException e) {
-                StringBuilder msg = new StringBuilder();
-                for (StackTraceElement element : e.getStackTrace()) {
-                    msg.append(element.toString()).append("\n");
-                }
-                getLogger().severe(msg.toString());
-            }
-        }
-        getLogger().info("Address: " + ip);
-    }
+public final class Lokeon extends Plugin {
 
     @Override
     public void onEnable() {
-        super.onEnable();
-        http = new Http(ip, getLogger());
-        getLogger().info("Sending hook");
-        http.hook();
-        PluginManager pm = getServer().getPluginManager();
-        EventListener eventListener = new EventListener(this);
-        pm.registerEvents(eventListener, this);
-        eventListener.startTimer();
+        // Plugin startup logic
     }
 
-    Http getHttp() { return http;}
-
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
 }
