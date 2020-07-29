43ef95e1ee3df93826a91b483883527e9ad602ad
Pythonic-Rainbow <wongchingho0@gmail.com>
2020-07-30 03:51:00 +0800

    Port Http to Lokeon API

---
+++ waterfall/src/main/java/chingdim/lokeon/LokeonWaterfall.java
@@ -0,0 +1,16 @@
+package chingdim.lokeon;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class LokeonWaterfall extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
